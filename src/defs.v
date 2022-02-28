68531ea999ea6eb9e1bbcec2ebd1c6734691f6c5
YMD-Glasses <reyu735+ymd_github@gmail.com>
2022-02-28 16:52:10 +0900

    暗黙の型宣言を禁止した．

--- src/defs.v
+++ src/defs.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 `define XLEN   32 // the width of an integer register
 `define IALIGN 32 // the instruction-address alignment constraint
 `define ILEN   32 // the maximum instruction length
