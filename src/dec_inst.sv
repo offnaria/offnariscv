2cd905748262a303354923e30398a8c7c477aecb
YMD-Glasses <reyu735+ymd_github@gmail.com>
2022-02-28 14:09:00 +0900

    命令デコーダをADDI,ADDに対応させた．

---
+++ src/dec_inst.sv
@@ -0,0 +1,30 @@
+`include "defs.v"
+`include "opcodes.v"
+`include "funct3s.v"
+
+// Instruction decoder
+
+module dec_inst (
+    input  logic[31:0] inst,
+
+    output logic[4:0]  rs1,
+    output logic[4:0]  rs2,
+    output logic[4:0]  rd,
+
+    output logic[2:0]  inst_format,
+
+    output logic[0:0]  is_addi
+    output logic[0:0]  is_add,
+);
+
+wire[6:0] opcode = inst[6:0];
+wire[2:0] funct3 = inst[14:12];
+
+assign rs1 = inst[19:15];
+assign rs2 = inst[24:20];
+assign rd  = inst[11:7];
+
+assign is_addi = (opcode=={OP-IMM, 2'b11} && funct3==ADDI);
+assign is_add  = (opcode=={OP, 2'b11} && funct3==ADD);
+
+endmodule
