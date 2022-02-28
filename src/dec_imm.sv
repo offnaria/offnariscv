b8928789759e9dc7fa83a3f4a6ae56e556b4b734
YMD-Glasses <reyu735+ymd_github@gmail.com>
2022-02-28 20:39:39 +0900

    マクロの修正．

--- src/dec_imm.sv
+++ src/dec_imm.sv
@@ -10,11 +10,11 @@ module dec_imm (
 
 always_comb begin
     case (inst_format)
-        I_TYPE: imm = {21{inst[31]}, inst[30:25], inst[24:21], inst[20]};
-        S_TYPE: imm = {21{inst[31]}, inst[30:25], inst[11:8],  inst[7]};
-        B_TYPE: imm = {20{inst[31]}, inst[7],     inst[30:25], inst[11:8], 1'b0};
-        U_TYPE: imm = {inst[31],     inst[30:20], inst[19:12], 12{1'b0}};
-        J_TYPE: imm = {12{inst[31]}, inst[19:12], inst[20],    inst[30:25], inst[24:21], 1'b0};
+        `I_TYPE: imm = {21{inst[31]}, inst[30:25], inst[24:21], inst[20]};
+        `S_TYPE: imm = {21{inst[31]}, inst[30:25], inst[11:8],  inst[7]};
+        `B_TYPE: imm = {20{inst[31]}, inst[7],     inst[30:25], inst[11:8], 1'b0};
+        `U_TYPE: imm = {inst[31],     inst[30:20], inst[19:12], 12{1'b0}};
+        `J_TYPE: imm = {12{inst[31]}, inst[19:12], inst[20],    inst[30:25], inst[24:21], 1'b0};
         default: imm = 32{1'b0};
     endcase
 end
