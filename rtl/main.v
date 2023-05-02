efaa5b44583aaf7303e2deb6131eb61f4c5e0b15
yamada <yamada@arch.cs.titech.ac.jp>
2023-05-02 18:35:46 +0900

    Add minimum program counter

--- rtl/main.v
+++ rtl/main.v
@@ -3,5 +3,15 @@ module m_proc (
     input wire w_rstn,
     output wire [31:0] w_rslt
 );
+
+    reg [31:0] r_pc = 0;
+
+    always @(posedge w_clk) begin
+        if (!w_rstn) begin
+            r_pc <= 0;
+        end else begin
+            r_pc <= r_pc + 4;
+        end
+    end
     
 endmodule
