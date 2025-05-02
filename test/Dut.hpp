8783a7a32e4c5d94a896a70747313b1ab33e9f06
offnaria <yamada@arch.cs.titech.ac.jp>
2025-05-02 22:40:29 +0900

    Add a template class for DUT.

---
+++ test/Dut.hpp
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+template <class T>
+class Dut {
+    T* dut_wrap;
+public:
+    Dut() {
+        dut_wrap = new T();
+    }
+    ~Dut() {
+        dut_wrap->final();
+        delete dut_wrap;
+    }
+
+    T* operator->() const noexcept {
+        return dut_wrap;
+    }
+    void step(int n = 1) {
+        for (int i = 0; i < n; i++) {
+            dut_wrap->clk = 0;
+            dut_wrap->eval();
+            dut_wrap->clk = 1;
+            dut_wrap->eval();
+        }
+    }
+    void reset(int n = 10) {
+        dut_wrap->rst_n = 0;
+        step(n);
+        dut_wrap->rst_n = 1;
+    }
+};
