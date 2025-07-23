950e044de538bdc268dcff10fb63fc477c1cc578
offnaria <yamada@arch.cs.titech.ac.jp>
2025-07-24 01:02:50 +0900

    Implement L1 Data cache that uses write-back policy.

--- src/lsu/lsu.sv
+++ src/lsu/lsu.sv
@@ -26,6 +26,7 @@ module lsu
   localparam BLOCK_SEL_WIDTH = $clog2(BLOCK_SIZE / XLEN);
   localparam INDEX_WIDTH = l1d_dir_if.INDEX_WIDTH;
   localparam TAG_WIDTH = l1d_dir_if.TAG_WIDTH;
+  localparam STRB_WIDTH = l1d_mem_if.STRB_WIDTH;
 
   // Assert conditions
   initial begin
@@ -40,188 +41,201 @@ module lsu
   end
 
   // Define types
-  typedef enum logic [2:0] {
+  typedef enum logic [1:0] {
     IDLE,
-    PTW,
-    LOAD,
-    STORE,
+    COMPARE,
     WAIT
   } state_e;  // TODO: There might be more states for AMO in the future
 
   // Declare interfaces
-  axis_if #(.TDATA_WIDTH($bits(rflsu_tdata_t))) rflsu_slice_if ();
   axis_if #(.TDATA_WIDTH($bits(lsuwb_tdata_t))) lsuwb_slice_if ();
+
+  // Define functions
+  function automatic logic [XLEN-1:0] slice_load(logic [BLOCK_SIZE-1:0] block, lsu_cmd_e cmd,
+                                                 logic [BLOCK_OFFSET_WIDTH-1:0] offset);
+    slice_load = '0;
+    case (cmd)
+      LSU_LW:  slice_load = block[XLEN*offset[2+:BLOCK_SEL_WIDTH]+:XLEN];
+      LSU_LH:  slice_load = XLEN'(signed'(block[16*offset[1+:BLOCK_SEL_WIDTH+1]+:16]));
+      LSU_LB:  slice_load = XLEN'(signed'(block[8*offset[0+:BLOCK_SEL_WIDTH+2]+:8]));
+      LSU_LHU: slice_load = XLEN'(unsigned'(block[16*offset[1+:BLOCK_SEL_WIDTH+1]+:16]));
+      LSU_LBU: slice_load = XLEN'(unsigned'(block[8*offset[0+:BLOCK_SEL_WIDTH+2]+:8]));
+      default: begin
+      end
+    endcase
+  endfunction
+
+  function automatic logic [STRB_WIDTH-1:0] get_strb(lsu_cmd_e cmd,
+                                                     logic [BLOCK_OFFSET_WIDTH-1:0] offset);
+    get_strb = '0;
+    case (cmd)
+      LSU_SW: get_strb[4*offset[2+:BLOCK_SEL_WIDTH]+:4] = '1;
+      LSU_SH: get_strb[2*offset[1+:BLOCK_SEL_WIDTH+1]+:2] = '1;
+      LSU_SB: get_strb[offset[0+:BLOCK_SEL_WIDTH+2]] = '1;
+      default: begin
+      end
+    endcase
+  endfunction
 
   // Declare registers and their next states
   state_e state_q, state_d;
+  logic rflsu_tready_q, rflsu_tready_d;
   logic arvalid_q, arvalid_d;
+  logic [ADDR_WIDTH-1:0] araddr_q, araddr_d;
   logic rready_q, rready_d;
   logic [BLOCK_SIZE-1:0] rdata_q, rdata_d;
   logic [$bits(lsu_ace_if.rresp)-1:0] rresp_q, rresp_d;
   logic awvalid_q, awvalid_d;
+  logic [ADDR_WIDTH-1:0] awaddr_q, awaddr_d;
   logic wvalid_q, wvalid_d;
   logic [BLOCK_SIZE-1:0] wdata_q, wdata_d;
-  logic [BLOCK_SIZE/8-1:0] wstrb_q, wstrb_d;
+  logic [BLOCK_SIZE/8-1:0] wstrb_q, wstrb_d;  // For cache update
   logic bready_q, bready_d;
   logic [$bits(lsu_ace_if.bresp)-1:0] bresp_q, bresp_d;
-  logic [ADDR_WIDTH-1:0] araddr_q, araddr_d;
-  logic [ADDR_WIDTH-1:0] awaddr_q, awaddr_d;
-  logic l1dc_hit_q, l1dc_hit_d;
+  logic [TAG_WIDTH-1:0] tag_q, tag_d;
+  logic [INDEX_WIDTH-1:0] index_q, index_d;
+  logic load_q, load_d;
+  logic store_q, store_d;
+  lsu_cmd_e cmd_q, cmd_d;
+  logic [XLEN-1:0] op2_q, op2_d;
 
   logic [INDEX_WIDTH-1:0] l1dc_dir_index_q, l1dc_dir_index_d;
   logic [INDEX_WIDTH-1:0] l1dc_mem_index_q, l1dc_mem_index_d;
 
   // Declare wires
-  logic rflsu_ack;
-  rflsu_tdata_t rflsu_if_tdata, rflsu_slice_tdata;
-  lsuwb_tdata_t lsuwb_tdata;
+  rflsu_tdata_t rflsu_tdata;
   logic [XLEN-1:0] effective_addr;
   logic l1dtlb_hit;
-  logic [TAG_WIDTH-1:0] tag;
-  logic [((BLOCK_SEL_WIDTH>0)?BLOCK_SEL_WIDTH : 1)-1:0] block_sel;
+  lsuwb_tdata_t lsuwb_tdata;
+  logic [BLOCK_SIZE-1:0] store_data;
 
-  assign rflsu_ack = rflsu_axis_if.tvalid && rflsu_axis_if.tready;
+  assign rflsu_axis_if.tready = rflsu_tready_q;
 
   always_comb begin
     state_d = state_q;
     arvalid_d = arvalid_q;
+    araddr_d = araddr_q;
     rready_d = rready_q;
-    awvalid_d = awvalid_q;
     rdata_d = rdata_q;
     rresp_d = rresp_q;
+    awvalid_d = awvalid_q;
+    awaddr_d = awaddr_q;
     wvalid_d = wvalid_q;
     wdata_d = wdata_q;
     wstrb_d = wstrb_q;
     bready_d = bready_q;
     bresp_d = bresp_q;
-    araddr_d = araddr_q;
-    awaddr_d = awaddr_q;
-    l1dc_hit_d = l1dc_hit_q;
+    tag_d = tag_q;
+    index_d = index_q;
+    load_d = load_q;
+    store_d = store_q;
+    cmd_d = cmd_q;
+    op2_d = op2_q;
 
-    rflsu_if_tdata = rflsu_axis_if.tdata;
-    rflsu_slice_tdata = rflsu_slice_if.tdata;
-    lsuwb_tdata = '0;
+    l1dc_dir_index_d = l1dc_dir_index_q;
+    l1dc_mem_index_d = l1dc_mem_index_q;
 
-    lsuwb_slice_if.tvalid = '0;
-    rflsu_slice_if.tready = '0;
-
-    effective_addr = rflsu_if_tdata.operands.op1 + rflsu_if_tdata.offset;
+    rflsu_tdata = rflsu_axis_if.tdata;
+    effective_addr = rflsu_tdata.operands.op1 + rflsu_tdata.offset;
     l1dtlb_hit = 1'b1;  // TODO
-
-    if (rflsu_ack) begin
-      araddr_d = effective_addr;
-      awaddr_d = effective_addr;
-    end
-
-    tag = araddr_q[ADDR_WIDTH-1:ADDR_WIDTH-TAG_WIDTH];
-    block_sel = araddr_q[BLOCK_OFFSET_WIDTH-1-:BLOCK_SEL_WIDTH];
-
+    lsuwb_tdata = '0;
+    lsuwb_tdata.result = slice_load(l1d_mem_if.rdata, cmd_q, araddr_q[BLOCK_OFFSET_WIDTH-1:0]);
     l1d_dir_if.index = l1dc_dir_index_q;
-    l1d_mem_if.index = l1dc_mem_index_q;
-    l1d_dir_if.next_tag = tag;
+    l1d_dir_if.next_tag = tag_q;
     l1d_dir_if.next_state = '{default: '0, v: 1'b1};
     l1d_dir_if.write = '0;
+    lsuwb_slice_if.tvalid = '0;
+
+    if (lsu_ace_if.arready) begin  // AR channel
+      arvalid_d = '0;
+    end
+    if (lsu_ace_if.rvalid) begin  // R channel
+      rready_d = '0;
+      rdata_d  = lsu_ace_if.rdata;
+      rresp_d  = lsu_ace_if.rresp;
+    end
+    if (lsu_ace_if.awready) begin  // AW channel
+      awvalid_d = '0;
+    end
+    if (lsu_ace_if.wready) begin  // W channel
+      wvalid_d = '0;
+    end
+    if (lsu_ace_if.bvalid) begin  // B channel
+      bready_d = '0;
+      bresp_d  = lsu_ace_if.bresp;
+    end
+
+    store_data = '0;
+    unique case (cmd_q)
+      LSU_SW: store_data = {(BLOCK_SIZE / 32) {op2_q}};
+      LSU_SH: store_data = {(BLOCK_SIZE / 16) {op2_q[15:0]}};
+      LSU_SB: store_data = {(BLOCK_SIZE / 8) {op2_q[7:0]}};
+      default: begin
+      end
+    endcase
+
+    l1d_mem_if.index = l1dc_mem_index_q;
     l1d_mem_if.wstrb = '0;
+    l1d_mem_if.wdata = rdata_d;
+    if (store_q) begin
+      l1d_dir_if.next_state.d = 1'b1;
+      for (int i = 0; i < STRB_WIDTH; i++) begin
+        if (wstrb_q[i]) begin
+          l1d_mem_if.wdata[8*i+:8] = store_data[8*(i%4)+:8];
+        end
+      end
+    end
 
     unique case (state_q)
       IDLE: begin
-        if (rflsu_slice_if.tvalid && !invalidate) begin
-          if (l1dtlb_hit) begin
-            if (l1d_dir_if.current_state.v) begin
-              if (l1d_dir_if.current_tag == tag) begin  // Hit
-                lsuwb_slice_if.tvalid = 1'b1;
-                if (lsuwb_slice_if.tready) begin
-                  rflsu_slice_if.tready = 1'b1;
-                end
-              end else begin  // Miss but not empty
-                // TODO: Evict the block
+        araddr_d = effective_addr;
+        tag_d = effective_addr[ADDR_WIDTH-1-:TAG_WIDTH];
+        index_d = effective_addr[BLOCK_OFFSET_WIDTH+:INDEX_WIDTH];
+        load_d = rflsu_tdata.cmd inside {LSU_LW, LSU_LH, LSU_LB, LSU_LHU, LSU_LBU};
+        store_d = rflsu_tdata.cmd inside {LSU_SW, LSU_SH, LSU_SB};
+        cmd_d = rflsu_tdata.cmd;
+        op2_d = rflsu_tdata.operands.op2;
+        l1dc_dir_index_d = effective_addr[BLOCK_OFFSET_WIDTH+:INDEX_WIDTH];
+        l1dc_mem_index_d = effective_addr[BLOCK_OFFSET_WIDTH+:INDEX_WIDTH];
+        if (store_d) begin
+          wstrb_d = get_strb(cmd_d, effective_addr[BLOCK_OFFSET_WIDTH-1:0]);
+        end
+        if (rflsu_axis_if.tvalid && !invalidate) begin
+          state_d = COMPARE;
+        end
+      end
+      COMPARE: begin
+        if (l1dtlb_hit) begin
+          if (l1d_dir_if.current_state.v && (l1d_dir_if.current_tag == tag_q)) begin  // Hit
+            lsuwb_slice_if.tvalid = 1'b1;
+            if (lsuwb_slice_if.tready) begin
+              if (store_q) begin
+                l1d_dir_if.write = 1'b1;
+                l1d_mem_if.wstrb = wstrb_q;
               end
-            end else begin  // Miss and empty
-              if (rflsu_slice_tdata.cmd inside {LSU_LW, LSU_LH, LSU_LB, LSU_LHU, LSU_LBU}) begin
-                arvalid_d = 1'b1;
-                rready_d  = 1'b1;
-                state_d   = LOAD;
-              end
-              if (rflsu_slice_tdata.cmd inside {LSU_SW, LSU_SH, LSU_SB}) begin
-                awvalid_d = 1'b1;
-                wvalid_d  = 1'b1;
-                unique case (rflsu_slice_tdata.cmd)
-                  LSU_SW: wdata_d = {(BLOCK_SIZE / 32) {rflsu_slice_tdata.operands.op2}};
-                  LSU_SH: wdata_d = {(BLOCK_SIZE / 16) {rflsu_slice_tdata.operands.op2[15:0]}};
-                  LSU_SB: wdata_d = {(BLOCK_SIZE / 8) {rflsu_slice_tdata.operands.op2[7:0]}};
-                  default: begin
-                  end
-                endcase
-                wstrb_d = '0;
-                for (int i = 0; i < BLOCK_SIZE / 8; ++i) begin
-                  unique case (rflsu_slice_tdata.cmd)
-                    LSU_SW: if (block_sel == BLOCK_SEL_WIDTH'(i / 4)) wstrb_d[i] = 1'b1;
-                    LSU_SH:
-                    if (awaddr_q[BLOCK_OFFSET_WIDTH-1:1] == (BLOCK_OFFSET_WIDTH - 1)'(i / 2))
-                      wstrb_d[i] = 1'b1;
-                    LSU_SB:
-                    if (awaddr_q[BLOCK_OFFSET_WIDTH-1:0] == BLOCK_OFFSET_WIDTH'(i))
-                      wstrb_d[i] = 1'b1;
-                    default: begin
-                    end
-                  endcase
-                end
-                bready_d = 1'b1;
-                state_d  = STORE;
-              end
-              // TODO: AMO
+              state_d = IDLE;
             end
+          end else begin  // Miss
+            arvalid_d = 1'b1;
+            rready_d  = 1'b1;
+            awaddr_d  = {l1d_dir_if.current_tag, index_q, BLOCK_OFFSET_WIDTH'(0)};
+            wdata_d   = l1d_mem_if.rdata;
+            if (l1d_dir_if.current_state.v && l1d_dir_if.current_state.d) begin  // Write back
+              awvalid_d = 1'b1;
+              wvalid_d  = 1'b1;
+              bready_d  = 1'b1;
+            end
+            state_d = WAIT;
           end
         end
       end
-      PTW: begin
-        // TODO
-      end
-      LOAD: begin
-        if (lsu_ace_if.arready) begin
-          arvalid_d = '0;
-        end
-        if (lsu_ace_if.rvalid) begin
-          rready_d = '0;
-          rresp_d  = lsu_ace_if.rresp;
-          rdata_d  = lsu_ace_if.rdata;
-        end
-        if (!rready_d) begin
-          unique case (rflsu_slice_tdata.cmd)
-            LSU_LW: lsuwb_tdata.result = rdata_d[block_sel*XLEN+:XLEN];
-            LSU_LH:
-            lsuwb_tdata.result = XLEN'(signed'(rdata_d[16*araddr_q[BLOCK_OFFSET_WIDTH-1:1]+:16]));
-            LSU_LHU:
-            lsuwb_tdata.result = XLEN'(unsigned'(rdata_d[16*araddr_q[BLOCK_OFFSET_WIDTH-1:1]+:16]));
-            LSU_LB:
-            lsuwb_tdata.result = XLEN'(signed'(rdata_d[8*araddr_q[BLOCK_OFFSET_WIDTH-1:0]+:8]));
-            LSU_LBU:
-            lsuwb_tdata.result = XLEN'(unsigned'(rdata_d[8*araddr_q[BLOCK_OFFSET_WIDTH-1:0]+:8]));
-            default: begin
-            end
-          endcase
+      WAIT: begin
+        if (!rready_d && !bready_d) begin
           lsuwb_slice_if.tvalid = 1'b1;
+          lsuwb_tdata.result = slice_load(rdata_d, cmd_q, araddr_q[BLOCK_OFFSET_WIDTH-1:0]);
           if (lsuwb_slice_if.tready) begin
-            rflsu_slice_if.tready = 1'b1;
-            state_d = IDLE;
-          end
-        end
-      end
-      STORE: begin
-        if (lsu_ace_if.awready) begin
-          awvalid_d = '0;
-        end
-        if (lsu_ace_if.wready) begin
-          wvalid_d = '0;
-        end
-        if (lsu_ace_if.bvalid) begin
-          bready_d = '0;
-          bresp_d  = lsu_ace_if.bresp;
-        end
-        if (!bready_d) begin
-          lsuwb_slice_if.tvalid = 1'b1;
-          if (lsuwb_slice_if.tready) begin
-            rflsu_slice_if.tready = 1'b1;
+            l1d_dir_if.write = 1'b1;
+            l1d_mem_if.wstrb = '1;
             state_d = IDLE;
           end
         end
@@ -230,19 +244,20 @@ module lsu
       end
     endcase
 
+    rflsu_tready_d = (state_d == IDLE);
+
 `ifndef SYNTHESIS
     lsuwb_tdata.addr  = araddr_q;
-    lsuwb_tdata.wdata = rflsu_slice_tdata.operands.op2;
-    lsuwb_tdata.store = (rflsu_slice_tdata.cmd == LSU_SW);
+    lsuwb_tdata.wdata = op2_q;
+    lsuwb_tdata.store = store_q;
 `endif
-
     lsuwb_slice_if.tdata = lsuwb_tdata;
-
   end
 
   always_ff @(posedge clk) begin
     if (rst) begin
       state_q <= IDLE;
+      rflsu_tready_q <= 0;
       arvalid_q <= 0;
       rready_q <= 0;
       rdata_q <= '0;
@@ -255,9 +270,15 @@ module lsu
       bresp_q <= '0;
       araddr_q <= '0;
       awaddr_q <= '0;
-      l1dc_hit_q <= 0;
+      tag_q <= '0;
+      index_q <= '0;
+      load_q <= 0;
+      store_q <= 0;
+      cmd_q <= LSU_LW;
+      op2_q <= '0;
     end else begin
       state_q <= state_d;
+      rflsu_tready_q <= rflsu_tready_d;
       arvalid_q <= arvalid_d;
       rready_q <= rready_d;
       rdata_q <= rdata_d;
@@ -270,30 +291,40 @@ module lsu
       bresp_q <= bresp_d;
       araddr_q <= araddr_d;
       awaddr_q <= awaddr_d;
-      l1dc_hit_q <= l1dc_hit_d;
+      tag_q <= tag_d;
+      index_q <= index_d;
+      load_q <= load_d;
+      store_q <= store_d;
+      cmd_q <= cmd_d;
+      op2_q <= op2_d;
       $write(
           "LSU: state=%s, arvalid=%b, rready=%b, awvalid=%b, wvalid=%b, wdata=0x%h, wstrb=0x%h, bready=%b, bresp=0x%h, addr=0x%h\n",
           state_q.name(), arvalid_q, rready_q, awvalid_q, wvalid_q, wdata_q, wstrb_q, bready_q,
           bresp_q, araddr_q);
+      if ((state_q == COMPARE) && l1dtlb_hit && l1d_dir_if.current_state.v && (l1d_dir_if.current_tag == tag_q))
+        $write(
+            "LSU: Cache hit... araddr=0x%h, tag=0x%h, index=0x%h, rdata=0x%h\n",
+            araddr_q,
+            tag_q,
+            index_q,
+            l1d_mem_if.rdata
+        );
+      if (|l1d_mem_if.wstrb)
+        $write(
+            "LSU: Writing to memory... wstrb=%b, wdata=0x%h, wstrb_q=%b\n",
+            l1d_mem_if.wstrb,
+            l1d_mem_if.wdata,
+            wstrb_q
+        );
     end
   end
 
   always_ff @(posedge clk) begin
-    if (rflsu_ack) begin
-      l1dc_dir_index_q <= effective_addr[BLOCK_OFFSET_WIDTH+:INDEX_WIDTH];
-      l1dc_mem_index_q <= effective_addr[BLOCK_OFFSET_WIDTH+:INDEX_WIDTH];
-    end
+    l1dc_dir_index_q <= l1dc_dir_index_d;
+    l1dc_mem_index_q <= l1dc_mem_index_d;
   end
 
-  axis_slice rflsu_slice_inst (
-      .clk(clk),
-      .rst(rst),
-      .axis_mif(rflsu_slice_if),
-      .axis_sif(rflsu_axis_if),
-      .invalidate(invalidate)
-  );
-
-  axis_slice lsuwb_slice_inst (
+  axis_skid_buffer lsuwb_slice_inst (
       .clk(clk),
       .rst(rst),
       .axis_mif(lsuwb_axis_if),
@@ -322,7 +353,7 @@ module lsu
   //// W channel signals
   assign lsu_ace_if.wvalid = wvalid_q;
   assign lsu_ace_if.wdata = wdata_q;
-  assign lsu_ace_if.wstrb = wstrb_q;
+  assign lsu_ace_if.wstrb = '1;  // For write back
   assign lsu_ace_if.wlast = 1'b1;  // TODO
   assign lsu_ace_if.wuser = '0;  // TODO
 
